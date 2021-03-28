e7ab5c232d26bc0dc7fd133fac77b4635570d44e
worckyky <mymailtolaaa@gmal.com>
2021-03-29 01:33:46 +0300

    deploy changes

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,6 +1,12 @@
+
+
+
 export default {
   // Target: https://go.nuxtjs.dev/config-target
   target: 'static',
+  router: {
+    base: '/<Kazanmarathon>/'
+  },
 
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
