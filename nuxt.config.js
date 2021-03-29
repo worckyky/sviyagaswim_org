befd5d0b6eaab8216550e964d0281f66784fb240
worckyky <mymailtolaaa@gmal.com>
2021-03-29 12:58:04 +0300

    fix nuxt config

--- nuxt.config.js
+++ nuxt.config.js
@@ -5,7 +5,7 @@ export default {
   // Target: https://go.nuxtjs.dev/config-target
   target: 'static',
   router: {
-    base: '/<Kazanmarathon>/'
+    base: '/Kazanmarathon/'
   },
 
   // Global page headers: https://go.nuxtjs.dev/config-head
