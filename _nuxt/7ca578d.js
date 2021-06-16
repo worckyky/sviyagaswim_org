0f2f9622949362f618811e1a1720ff734fd2ffd9
Anatoly Mitroshin <amitroshin@hq.fix.ru>
2021-06-16 19:32:59 +0300

    55b497d

---
+++ _nuxt/7ca578d.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[24],{382:function(n,e,d){"use strict";d.r(e);var l={},o=d(24),component=Object(o.a)(l,undefined,undefined,!1,null,null,null);e.default=component.exports}}]);
